7c139594e5c8a76b2de114b8b40fb9dad559f702
Poojitha1234520 <22bk1a12a0@stpetershyd.com>
2025-02-20 12:47:58 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import DisplayAll from "./components/DisplayAll"
-import AddStudent from "./components/AddStudent"
-import DeleteStudent from "./components/DeleteStudent"
-import SearchStudent from "./components/SearchStudent"
 
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DisplayAll from "./components/DisplayAll";
+import AddStudent from "./components/AddStudent";
+import DeleteStudent from "./components/DeleteStudent";
+import SearchStudent from "./components/SearchStudent";
+import AddCar from "./components/AddCar";
 
 export default function App() {
   return (
@@ -17,10 +16,10 @@ export default function App() {
           <Route path="/s-add" element={<AddStudent />} />
           <Route path="/del" element={<DeleteStudent />} />
           <Route path="/search" element={<SearchStudent />} />
-
-
+          <Route path="/addCar" element={<AddCar />} />
+        
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
